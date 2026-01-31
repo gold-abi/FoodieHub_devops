603be31403f9439c16d39f0f770c3a7c0a7a10f7
gold-abi <p.thangaabirami@gmail.com>
2026-01-31 19:18:28 +0530

    committed

--- online_food/includes/config.php
+++ online_food/includes/config.php
@@ -3,11 +3,13 @@ if (session_status() === PHP_SESSION_NONE) {
     session_start();
 }
 
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "food_order_db";  // Must match your SQL
+$servername = getenv("DB_HOST");
+$username   = getenv("DB_USER");
+$password   = getenv("DB_PASS");
+$dbname     = getenv("DB_NAME");
+
 $conn = mysqli_connect($servername, $username, $password, $dbname);
+
 
 if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
